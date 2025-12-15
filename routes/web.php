acb626513b263216d35a49e883cf8c2e561d164f
Bilan H <bilanhersi02@gmail.com>
2025-12-15 19:48:28 +0000

    Added controllers

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FeedController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FollowController;
+use App\Http\Controllers\HashtagController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', fn () => redirect()->route('feed'));
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/feed', [FeedController::class, 'index'])->name('feed');
+
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+
+    Route::get('/users/{user}', [ProfileController::class, 'show'])->name('users.show');
+
+    Route::post('/users/{user}/follow', [FollowController::class, 'store'])->name('follow.store');
+    Route::delete('/users/{user}/follow', [FollowController::class, 'destroy'])->name('follow.destroy');
+
+    Route::get('/hashtags/{hashtag:slug}', [HashtagController::class, 'show'])->name('hashtags.show');
 });
+
